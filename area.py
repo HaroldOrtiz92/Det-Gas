5c0193c464c672e5bd1ed019535f8add8bcb8472
HaroldOrtiz <ortizriveris@everis.com>
2023-01-16 19:01:46 -0500

    Cuarta modificacion

--- area.py
+++ area.py
@@ -1,3 +1,18 @@
 
 def areaCuadrado():
-    pass
+    lado = float(input("Ingrese el valor del lado: "))
+    return print("El area del cuadrado es: ", lado*lado)
+areaCuadrado() # 17 de Julio del 2023
+
+def areaRectangulo():
+    base = float(input("Ingrese el valor de la base: "))
+    altura = float(input("Ingrese el valor de la altura"))
+    return print("El area del rectangulo es: ", base * altura)
+areaRectangulo() # 18 de Julio del 2023
+
+pi = 3.1416
+
+def areaCirculo():
+    radio = float(input("Ingrese el valor del radio: "))
+    return print("El area del rectangulo es: ", pi *radio **2)
+areaCirculo() # 18 de Julio del 2023
